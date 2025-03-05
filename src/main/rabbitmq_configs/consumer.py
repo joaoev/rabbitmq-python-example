5b9a8105bd6839d7498e13cb6d002164a3d2c8e5
joaoev <joaoalvesxp@alu.ufc.br>
2025-03-04 22:27:28 -0300

    feat: implementando consumer

---
+++ src/main/rabbitmq_configs/consumer.py
@@ -0,0 +1,41 @@
+import pika
+
+
+def rabbitmq_callback(ch, method, properties, body):
+    print(f"Received message: {body}")
+
+
+class RabbitMQConsumer:
+    def __init__(self) -> None:
+        self.__host = "localhost"
+        self.__port = 5672
+        self.__username = "guest"
+        self.__password = "guest"
+        self.__queue = "test_queue"
+        self.__channel = self.create_channel()
+
+    def create_channel(self):
+        connection_parameters = pika.ConnectionParameters(
+            host=self.__host,
+            port=self.__port,
+            credentials=pika.PlainCredentials(
+                username=self.__username,
+                password=self.__password
+            ),
+        )
+
+        channel = pika.BlockingConnection(connection_parameters).channel()
+        channel.queue_declare(
+            queue=self.__queue,
+            durable=True
+        )
+        channel.basic_consume(
+            queue=self.__queue,
+            auto_ack=True,
+            on_message_callback=rabbitmq_callback
+        )
+
+        return channel
+
+    def start(self):
+        self.__channel.start_consuming()
